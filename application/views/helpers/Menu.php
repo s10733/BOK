0b7eb4269709317dc901d96c765ff669ca39ee3f
dzwonek <dzwonek5@o2.pl>
2015-01-20 01:01:39 +0100

    BOK

---
+++ application/views/helpers/Menu.php
@@ -0,0 +1,20 @@
+<?php
+
+class Zend_View_Helper_Menu  extends Zend_View_Helper_Abstract{
+    
+    public function menu(){
+     
+        
+// Pobieramy informacje czy ma prawa admina
+        
+        
+        
+        $DbMenu = new Application_Model_DbTable_Menu();
+        $select = $DbMenu->select()
+                ->from('menu')
+                ->order('id');
+        $menu = $DbMenu->fetchAll($select);
+        return $this->view->partial('menu.phtml', array('menu'=>$menu));
+    }
+    
+}
